efacfb07f7736d3c046ec0b4c2d9c2a9235d73fd
Nonus <nonus@nonus.net>
2016-11-29 12:55:41 +0100

    Barebone ready!

---
+++ Advent/DayTemplate/Day.cs
@@ -0,0 +1,19 @@
+﻿#region
+
+using System;
+using Helper;
+
+#endregion
+
+namespace Advent
+{
+    internal static class Day
+    {
+        private static void Main()
+        {
+            var input = Utilities.GetInput(1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Advent/Helper/Utilities.cs
@@ -0,0 +1,34 @@
+﻿#region
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+#endregion
+
+namespace Helper
+{
+    public static class Utilities
+    {
+        public static string GetInput(uint day)
+        {
+            var separator = Path.DirectorySeparatorChar;
+            var filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"..{separator}..{separator}..{separator}Inputs{separator}Day{day}.txt");
+
+            if (!File.Exists(filepath))
+                throw new FileNotFoundException($"Could not find {filepath}");
+
+            string input;
+            using (var fs = new StreamReader(filepath))
+                input = fs.ReadToEnd();
+
+            Debug.Assert(!string.IsNullOrWhiteSpace(input), $"Day{day}.txt is empty.");
+
+            Console.WriteLine("------- Start of input -------");
+            Console.WriteLine(input);
+            Console.WriteLine("-------  End of input  -------");
+
+            return input;
+        }
+    }
+}

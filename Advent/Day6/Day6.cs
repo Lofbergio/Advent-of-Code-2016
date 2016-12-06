b7fcf39974969005804cb934eefbbc542f49b9a2
Nonus <nonus@nonus.net>
2016-12-06 08:48:28 +0100

    Day6 is the calm before the storm :1234:

---
+++ Advent/Day6/Day6.cs
@@ -0,0 +1,45 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Helper;
+using static System.Console;
+
+#endregion
+
+namespace Day6
+{
+    internal static class Day6
+    {
+        [STAThread]
+        private static void Main()
+        {
+            var input = Utilities.GetInput(6).Replace("\r\n", "").ToCharArray();
+            var message = string.Empty;
+            var reverseMessage = string.Empty;
+
+            for (var r = 0; r < 8; r++)
+            {
+                var charColumn = new List<char>();
+
+                for (var c = r; c < input.Length; c += 8)
+                    charColumn.Add(input[c]);
+
+                var orderedList = charColumn.GroupBy(c => c).OrderByDescending(c => c.Count());
+
+                message += orderedList.First().Key;
+                reverseMessage += orderedList.Last().Key;
+            }
+
+            WriteLine($"First part answer: {message}");
+            Clipboard.SetText(message);
+
+            WriteLine($"Second part answer: {reverseMessage}");
+            Clipboard.SetText(reverseMessage);
+
+            ReadKey();
+        }
+    }
+}
